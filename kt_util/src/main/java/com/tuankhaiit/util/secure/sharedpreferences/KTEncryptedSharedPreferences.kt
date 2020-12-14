515a3bad628e8f2a46529bc32dbe40b5f7c73853
Khai.TranDT <khai.tdt@homecredit.vn>
2020-12-14 14:12:01 +0700

    + Encrypted SharedPreferences

--- kt_util/src/main/java/com/tuankhaiit/util/secure/sharedpreferences/KTEncryptedSharedPreferences.kt
+++ kt_util/src/main/java/com/tuankhaiit/util/secure/sharedpreferences/KTEncryptedSharedPreferences.kt
@@ -27,8 +27,8 @@ class KTEncryptedSharedPreferences private constructor(
 
     override fun putInt(key: String, value: Int) = sharedPreferences.putInt(key, value)
 
-
     override fun getInt(key: String?, defValue: Int): Int = sharedPreferences.getInt(key, defValue)
+
     override fun putLong(key: String, value: Long) = sharedPreferences.putLong(key, value)
 
     override fun getLong(key: String?, defValue: Long): Long =
@@ -44,6 +44,28 @@ class KTEncryptedSharedPreferences private constructor(
 
     override fun getStringSet(key: String?, defValues: MutableSet<String>?): MutableSet<String>? =
         sharedPreferences.getStringSet(key, defValues)
+
+    inline fun <reified D : Any?> put(key: String, value: D?) {
+        when (value) {
+            is Boolean -> putBoolean(key, value)
+            is Float -> putFloat(key, value)
+            is Int -> putInt(key, value)
+            is Long -> putLong(key, value)
+            is String -> putString(key, value)
+            else -> putObject(key, value)
+        }
+    }
+
+    inline fun <reified T> get(key: String): T? {
+        return when (T::class.java) {
+            Boolean::javaClass -> getBoolean(key, false) as T?
+            Float::javaClass -> getFloat(key, 0.0f) as T?
+            Int::javaClass -> getInt(key, -1) as T?
+            Long::javaClass -> getLong(key, -1) as T?
+            String::javaClass -> getString(key, null) as T?
+            else -> getObject<T>(key)
+        }
+    }
 
     inline fun <reified T> putObject(key: String, obj: T?) {
         try {
